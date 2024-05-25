d69cf5c785f4f3a4549a8701b4c8d86a0ad73849
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-25 18:05:02 -0300

    Staging

--- src/app/core/components/sidenav.component.ts
+++ src/app/core/components/sidenav.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ToolbarComponent } from "./toolbar.component";
+import { AsideComponent } from "./aside.component";
 
 @Component({
-  selector: 'app-sidenav',
-  standalone: true,
-  imports: [RouterLink],
-  templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.scss'
+    selector: 'app-sidenav',
+    standalone: true,
+    templateUrl: './sidenav.component.html',
+    styleUrl: './sidenav.component.scss',
+    imports: [ToolbarComponent, AsideComponent]
 })
 export class SidenavComponent {
 
