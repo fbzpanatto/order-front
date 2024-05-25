a2958a239239c5e2f2a57311947e69df6c093bc3
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-25 17:30:39 -0300

    Staging

---
+++ src/app/features/customers/components/customers-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customers-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './customers-form.component.html',
+  styleUrl: './customers-form.component.scss'
+})
+export class CustomersFormComponent {
+
+}
