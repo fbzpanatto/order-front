36f621be473516053d70bd7cce95e5d2fc65a846
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-25 17:54:34 -0300

    Staging

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
-import { OrdersComponent } from './features/3_orders/orders.component';
+import { HomeComponent } from './features/1_home/home.component';
 import { CustomersComponent } from './features/2_customers/customers.component';
+import { OrdersComponent } from './features/3_orders/orders.component';
 
 export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'orders', component: OrdersComponent },
   { path: 'customers', component: CustomersComponent }
 ];
