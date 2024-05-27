b9e5fb3b5d112b67df0379cbe0185c77fe5ad265
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-27 15:00:35 -0300

    Staging

--- src/app/features/customers/components/animation.ts
+++ src/app/features/customers/components/animation.ts
@@ -1,16 +1,20 @@
 import { animate, state, style, transition, trigger } from "@angular/animations";
 
 export const ConditionAnimation = trigger('conditionalTrigger', [
-  state('shown', style({})),
+  // state('shown', style({})),
 
-  transition('void => shown', [
-    style({
-      opacity: 0
-    }),
-    animate(300, style({ opacity: 1 }))
-  ]),
+  // transition('void => shown', [
+  //   style({
+  //     opacity: 0
+  //   }),
+  //   animate(300, style({ opacity: 1 }))
+  // ]),
 
-  transition('shown => void', [
-    animate(300, style({ opacity: 0 }))
-  ])
+  // transition('shown => void', [
+  //   animate(300, style({ opacity: 0 }))
+  // ])
+
+  state('void', style({ transform: 'translateX(-120%)' })),
+  state('open', style({transform : 'translateX(0)'})),
+  transition('shown <=> void', [animate('1s ease-in')])
 ])
