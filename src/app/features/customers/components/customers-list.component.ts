cbcdb65e485053f0c89b596cd92b274a149d97ad
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-27 10:43:18 -0300

    Staging

--- src/app/features/customers/components/customers-list.component.ts
+++ src/app/features/customers/components/customers-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AsideComponent } from "../../../core/components/aside.component";
 
 @Component({
-  selector: 'app-customers-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './customers-list.component.html',
-  styleUrl: './customers-list.component.scss'
+    selector: 'app-customers-list',
+    standalone: true,
+    templateUrl: './customers-list.component.html',
+    styleUrl: './customers-list.component.scss',
+    imports: [AsideComponent]
 })
 export class CustomersListComponent {
 
