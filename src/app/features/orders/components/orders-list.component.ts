1c5b9c53023e7e1345ae66f516c443cb28d89463
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-27 11:15:25 -0300

    Staging

--- src/app/features/orders/components/orders-list.component.ts
+++ src/app/features/orders/components/orders-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AsideComponent } from "../../../core/components/aside.component";
 
 @Component({
-  selector: 'app-orders-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './orders-list.component.html',
-  styleUrl: './orders-list.component.scss'
+    selector: 'app-orders-list',
+    standalone: true,
+    templateUrl: './orders-list.component.html',
+    styleUrl: './orders-list.component.scss',
+    imports: [AsideComponent]
 })
 export class OrdersListComponent {
 
--- src/app/features/products/components/products-list.component.ts
+++ src/app/features/products/components/products-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AsideComponent } from "../../../core/components/aside.component";
 
 @Component({
-  selector: 'app-products-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './products-list.component.html',
-  styleUrl: './products-list.component.scss'
+    selector: 'app-products-list',
+    standalone: true,
+    templateUrl: './products-list.component.html',
+    styleUrl: './products-list.component.scss',
+    imports: [AsideComponent]
 })
 export class ProductsListComponent {
 
