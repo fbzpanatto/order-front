acd51819e7217039fada397594ee42da9d17a73d
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-27 11:17:45 -0300

    Staging

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AsideComponent } from "../../core/components/aside.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [AsideComponent]
 })
 export class HomeComponent {
 
