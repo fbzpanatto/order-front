2187ddbbc36ead6454017f4d8a1abdf01a34a989
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-28 10:34:43 -0300

    Staging

--- src/app/core/components/toolbar.component.ts
+++ src/app/core/components/toolbar.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AsideService } from '../../shared/services/aside.service';
+import { BackButtonDirective } from '../../shared/directives/backButton.directive';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, BackButtonDirective],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
