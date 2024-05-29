66aa866b52bcc6ae1038110a1db2ba6fe4cd5b44
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-29 14:06:19 -0300

    Staging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { SidenavComponent } from './core/components/sidenav.component';
+import { LoginComponent } from "./features/login/components/login.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, SidenavComponent, RouterLink]
+    imports: [RouterOutlet, SidenavComponent, RouterLink, LoginComponent]
 })
 export class AppComponent {
   title = 'front';
--- src/app/features/login/components/login.component.ts
+++ src/app/features/login/components/login.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  isPasswordType = true;
+  private fb = inject(FormBuilder)
+
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(4)]],
+    password: ['', [Validators.required, Validators.minLength(4)]]
+  });
+
+  login() {
+    
+  }
+
+  resetPassword() {
+
+  }
 }
