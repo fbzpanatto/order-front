ca3c7abe70ea667e393b021d3789f74eeab993ea
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-31 10:03:27 -0300

    Staging

--- src/app/features/home/components/home.component.ts
+++ src/app/features/home/components/home.component.ts
@@ -1,29 +1,31 @@
 import { Component, inject } from '@angular/core';
-import { AsideComponent } from '../../../core/components/aside.component';
 import { AsideService } from '../../../shared/services/aside.service';
 import { CommonModule } from '@angular/common';
-import { AsideConditionAnimation } from '../../../shared/animations/asideAnimation';
 import { ActivatedRoute } from '@angular/router';
-import { environment } from '../../../../environments/environment';
 import { ToolbarMenuService } from '../../../shared/services/toolbarMenu.service';
+import { environment } from '../../../../environments/environment';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss', '../../../styles/resource.scss'],
-    imports: [AsideComponent, CommonModule],
-    animations: [AsideConditionAnimation]
+    imports: [CommonModule]
 })
 export class HomeComponent {
 
-    private asideService = inject(AsideService)
-    private route = inject(ActivatedRoute)
-    private toolbarMenuService = inject(ToolbarMenuService)
+    #route = inject(ActivatedRoute)
+    #asideService = inject(AsideService)
+    #toolbarMenuService = inject(ToolbarMenuService)
 
     ngOnInit() {
-        this.toolbarMenuService.menuName = this.route.snapshot.data[environment.MENU]
+        this.menuSettings()
     }
 
-    get asideFlag() { return this.asideService.flag }
+    menuSettings() {
+        this.#toolbarMenuService.menuName = this.#route.snapshot.data[environment.MENU]
+        this.#toolbarMenuService.hasFilter = true
+    }
+
+    get asideFlag() { return this.#asideService.flag }
 }
