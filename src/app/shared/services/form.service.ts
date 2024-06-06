c0ecd3563cb33011099c4f49dcb99372c9b17683
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-06-06 15:34:47 -0300

    Staging

--- src/app/shared/services/form.service.ts
+++ src/app/shared/services/form.service.ts
@@ -6,27 +6,12 @@ import { FormGroup } from '@angular/forms';
 })
 export class FormService {
 
-  #originalForm?: { [key: string]: any }
-  #formGroup?: FormGroup
+  #original: FormGroup = new FormGroup({})
+  #currentF: FormGroup = new FormGroup({})
 
-  patchValues(values: any) { this.formGroup?.patchValue(values) }
+  get current() { return this.#currentF }
+  set current(form: FormGroup) { this.#currentF = form }
 
-  markAsPristineAndUntouched() {
-    this.#formGroup?.markAsPristine()
-    this.#formGroup?.markAsUntouched()
-  }
-
-  resetToOriginalValues() {
-    this.#formGroup?.patchValue(this.originalValues)
-    this.#formGroup?.markAsUntouched()
-    this.#formGroup?.markAsPristine()
-  }
-
-  get formValue() { return this.formGroup?.value }
-
-  get originalValues() { return this.#originalForm }
-  set originalValues(values: any) { this.#originalForm = values }
-
-  get formGroup(): FormGroup | undefined { return this.#formGroup }
-  set formGroup(form: FormGroup | undefined) { this.#formGroup = form }
+  get original() { return this.#original }
+  set original(form: FormGroup) { this.#original = form }
 }
