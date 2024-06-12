81436522aab6e5eed287aa233f944fbac895709e
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-06-12 20:49:03 -0300

    Staging

--- src/app/features/parameters/components/permissions/permissions-form.component.ts
+++ src/app/features/parameters/components/permissions/permissions-form.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormService } from '../../../../shared/services/form.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-permissions-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './permissions-form.component.html',
   styleUrls: ['../../../../styles/title-bar.scss', '../../../../styles/form.scss']
 })
 export class PermissionsFormComponent {
+
+  #router = inject(Router)
+  #fb = inject(FormBuilder)
+  #route = inject(ActivatedRoute)
+  #formService = inject(FormService)
+
+  form = this.#fb.group({
+  })
+
+  onSubmit(){
+
+  }
+
+  get command() { return this.#route.snapshot.paramMap.get('command') }
 
 }
