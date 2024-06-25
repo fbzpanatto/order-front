b27f2c0273c28a24576369b4acc257ad81ffce34
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-06-25 16:17:56 -0300

    Staging

--- src/app/features/customers/components/customers-form.component.ts
+++ src/app/features/customers/components/customers-form.component.ts
@@ -166,11 +166,11 @@ export class CustomersFormComponent implements OnDestroy {
 
   async onSubmit() {
     if (!this.idIsTrue) {
-      const response = await this.#customersHttp.saveData(this.currentValues)
+      const response = await this.#customersHttp.saveData(this.formDiff)
       if (!(response as SuccessPOST).affectedRows) { return }
       return this.redirect()
     }
-    const response = await this.#customersHttp.updateData({ company_id: parseInt(this.company_id as string), person_id: parseInt(this.person_id as string) }, this.currentValues)
+    const response = await this.#customersHttp.updateData({ company_id: parseInt(this.company_id as string), person_id: parseInt(this.person_id as string) }, this.formDiff)
     if (!(response as SuccessPATCH).affectedRows) { return }
     return this.redirect()
   }
